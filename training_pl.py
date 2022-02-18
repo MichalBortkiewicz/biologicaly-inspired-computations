8b0392b1156284c24f1f4d5628e602c2f4b0c2f6
Michal Bortkiewicz <michalbortkiewicz8@gmail.com>
2022-02-18 10:18:41 +0100

    Add training_pl.py with regularization

--- training_pl.py
+++ training_pl.py
@@ -30,7 +30,7 @@ class MLP(pl.LightningModule):
     def __init__(self, input_size=54, hidden_units=(32, 16), output_size=100):
         super().__init__()
         self.l1_strength = 0.0
-        self.l2_strength = 0.2
+        self.l2_strength = 0.0
 
         # new PL attributes:
         self.train_acc = Accuracy()
@@ -46,7 +46,9 @@ class MLP(pl.LightningModule):
         for hidden_unit in hidden_units:
             layer = nn.Linear(input_size, hidden_unit)
             all_layers.append(layer)
+            all_layers.append(nn.BatchNorm1d(num_features=hidden_unit))
             all_layers.append(nn.ReLU())
+            all_layers.append(nn.Dropout(0.3))
             input_size = hidden_unit
 
         all_layers.append(nn.Linear(hidden_units[-1], output_size))
@@ -70,6 +72,7 @@ class MLP(pl.LightningModule):
         self.train_acc.update(preds, y)
         self.train_f1.update(preds, y)
 
+        # maybe regularization
         if self.l1_strength > 0:
             l1_reg = sum(param.abs().sum() for param in self.parameters())
             loss += self.l1_strength * l1_reg
@@ -155,7 +158,7 @@ def create_dataloader_from_xy(x, y, shuffle=False, sampler=None) -> DataLoader:
 
 OUTPUT_PATH = os.path.join("artifacts", "outputs")
 RESULTS_PATH = os.path.join("artifacts", "results")
-MODEL_NAME_PREFIX = "mlp"
+MODEL_NAME_PREFIX = "mlp_reg"
 
 HIDDEN_UNITS_TO_CHECK = [
     [8],
@@ -171,6 +174,8 @@ HIDDEN_UNITS_TO_CHECK = [
     [8, 8, 8],
     [32, 16, 8],
     [16, 16, 16],
+    [128],
+    [1024, 256],
 ]
 
 if __name__ == "__main__":
@@ -188,8 +193,8 @@ if __name__ == "__main__":
 
     num_classes = max(mapping.keys()) + 1
 
-    for i in range(len(HIDDEN_UNITS_TO_CHECK)):
-    # for i in range(-1, 0, 1):
+    # for i in range(len(HIDDEN_UNITS_TO_CHECK)):
+    for i in range(-1, 0, 1):
         # K fold crossval
         kfold = KFold(shuffle=True, random_state=42)
 
@@ -199,6 +204,7 @@ if __name__ == "__main__":
         model_name = MODEL_NAME_PREFIX + f"_{model_config}"
 
         results = {}
+        results_train = {}
         for fold, (train_idx, valid_idx) in enumerate(kfold.split(x_original)):
             # Folder hack
             tb_logger = TensorBoardLogger(
@@ -244,7 +250,19 @@ if __name__ == "__main__":
             acc = accuracy_score(y_test, y_pred)
             results[fold] = {"test_acc": acc, "test_f1": f1}
 
+
+            y_pred = mlp.predict(torch.Tensor(x).cpu())
+            f1 = f1_score(y, y_pred, average="weighted")
+            acc = accuracy_score(y, y_pred)
+            results_train[fold] = {"train_acc": acc, "train_f1": f1}
+
         results_file_path = os.path.join(RESULTS_PATH, f"{model_name}.pkl")
         os.makedirs(os.path.dirname(results_file_path), exist_ok=True)
         with open(results_file_path, "wb") as results_file:
             pickle.dump(results, results_file)
+
+        # train
+        results_file_path = os.path.join(RESULTS_PATH, f"{model_name}_train.pkl")
+        os.makedirs(os.path.dirname(results_file_path), exist_ok=True)
+        with open(results_file_path, "wb") as results_file:
+            pickle.dump(results_train, results_file)
